893b99c63eddab874b843a62c9bb2f18964ec113
maxime-louis14 <maximelouis321@gmail.com>
2022-11-11 16:25:02 +0100

    IncrémentationEtDécrémentions DesStocksQuantit

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Materiel;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
 use App\Repository\ReservationRepository;
@@ -31,18 +31,27 @@ class ReservationController extends AbstractController
         $form->handleRequest($request);
 
         // Si m'ont formulaire et envoyait et valide, tu ajoutes une nouvelle date et tu enlèves une quantité à ce produit
+
         if ($form->isSubmitted() && $form->isValid()) {
             $reservation->setLoandate(new \DateTime());
+            $reservation->setIsrenderd(false);
             // Je veux incrémentation et décrémentation les stocks de produit dans l'entity matereil
             // (La manière la plus basique et naturelle est l’utilisation du ‘+1’ ou ‘-1’) -$a Négation Opposé de $a.
 
             // $quantity et egale a $reservation et qui recuper les donnés de product 
-            // et product recuper les donnés de Quantity et le -1 c'est 
-            $quantity = $reservation->getProduct()->getQuantity()-1;
-
+            // et product recuper les donnés de Quantity et le -1 retire une quantités
+            $quantity = $reservation->getProduct()->getQuantity() - 1;
             $reservation->getProduct()->setQuantity($quantity);
+            
+            /***
+             * En php orienté objet, on appelle ça les "getters" et "setters". Les fonctions "get"
+             * permettent de récupérer la valeur d'une propriété, alors que les fonctions "set" permettent d'initialiser la valeur d'une propriété.
+             */
+
             $reservationRepository->add($reservation, true);
             $reservationRepository->save($reservation, true);
+
+            // dd($reservation);
 
             return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -64,10 +73,27 @@ class ReservationController extends AbstractController
     #[Route('/{id}/edit', name: 'app_reservation_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Reservation $reservation, ReservationRepository $reservationRepository): Response
     {
+        //Je veux comparer mon ancien isIsrenderd au nouveaux
+        $oldereservation = $reservationRepository->find($reservation->getId());
+        //Ceci est là parce que ce n'est pas fou 
+        $oldrenderd = $oldereservation->isIsrenderd();
         $form = $this->createForm(ReservationType::class, $reservation);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Je veux incrémenter et décrémenter la quantité du produits
+            // Si mon produit et rendu tu me rajoure +1 a la quantité et si il n'est pas rendu tu -1
+            // tU FAIT PLUS 1 QUANT LE PRODUIT ET RENDU. 
+            if ($oldrenderd != $reservation->isIsrenderd()) {
+                
+                if ($reservation->isIsrenderd()) {
+                    $product = $reservation->getProduct()->getQuantity() + 1;
+                    $reservation->getProduct()->setQuantity($product);
+                } else {
+                    $quantity = $reservation->getProduct()->getQuantity() - 1;
+                    $reservation->getProduct()->setQuantity($quantity);
+                }
+            }
             $reservationRepository->add($reservation, true);
 
             return $this->redirectToRoute('app_reservation_index', [], Response::HTTP_SEE_OTHER);
