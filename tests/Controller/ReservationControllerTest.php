a42d2e99e694fba180c14a1efef411b17c48b668
maxime-louis14 <maximelouis321@gmail.com>
2022-11-12 15:22:07 +0100

    les test sont 200 sur materiel

--- tests/Controller/ReservationControllerTest.php
+++ tests/Controller/ReservationControllerTest.php
@@ -1,143 +1,139 @@
 <?php
 
-// namespace App\Test\Controller;
+namespace Test\Controller;
 
-// use App\Entity\Reservation;
-// use App\Repository\ReservationRepository;
-// use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-// use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Entity\Reservation;
+use App\Repository\ReservationRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-// class ReservationControllerTest extends WebTestCase
-// {
-//     private KernelBrowser $client;
-//     private ReservationRepository $repository;
-//     private string $path = '/reservation/';
+class ReservationControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private ReservationRepository $repository;
+    private string $path = '/reservation/';
 
-//     protected function setUp(): void
-//     {
-//         $this->client = static::createClient();
-//         $this->repository = (static::getContainer()->get('doctrine'))->getRepository(Reservation::class);
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = (static::getContainer()->get('doctrine'))->getRepository(Reservation::class);
 
-//         foreach ($this->repository->findAll() as $object) {
-//             $this->repository->remove($object, true);
-//         }
-//     }
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+    
+    //  je test si il y a une reponse 200 sur ma page index de reservation et si il y a bien ércit Reservation index.
+    public function testIndex(): void
+    {
+    $crawler = $this->client->request('GET', $this->path);
 
-//     //  je test si il y a une reponse 200 sur ma page index de reservation et si il y a bien ércit Reservation index.
-//     public function testIndex(): void
-//     {
-//     $crawler = $this->client->request('GET', '/reservation');
+        self::assertResponseStatusCodeSame(200);
+        
+    }
 
-//         self::assertResponseStatusCodeSame(200);
-//         self::assertPageTitleContains('Reservation index');
+    //  Ici c'est le test pour la création d'une nouvelle anonnce. qui return rien
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
 
-//         // Use the $crawler to perform additional assertions e.g.
-//         self::assertSame('Some text on the page', $crawler->filter('.p')->first());
-//     }
+        $this->client->request('GET', sprintf('%snew', $this->path));
 
-//     //  Ici c'est le test pour la création d'une nouvelle anonnce.
-//     public function testNew(): void
-//     {
-//         $originalNumObjectsInRepository = count($this->repository->findAll());
+        self::assertResponseStatusCodeSame(200);
+        
+        $this->client->submitForm('Save', [
+            'reservation[name]' => 'Testing',
+            'reservation[email]' => 'Testing',
+            'reservation[rendered]' => 'Testing',
+            'reservation[loandate]' => 'Testing',
+            'reservation[isrenderd]' => '',
+            'reservation[product]' => 'true false',
+        ]);
 
-//         $this->markTestIncomplete();
-//         $this->client->request('GET', sprintf('%snew', $this->path));
+        self::assertResponseRedirects('/reservation/');
 
-//         self::assertResponseStatusCodeSame(200);
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
 
-//         $this->client->submitForm('Save', [
-//             'reservation[name]' => 'Testing',
-//             'reservation[email]' => 'Testing',
-//             'reservation[rendered]' => 'Testing',
-//             'reservation[loandate]' => 'Testing',
-//             'reservation[isrenderd]' => 'Testing',
-//             'reservation[product]' => 'Testing',
-//         ]);
+    // public function testShow(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Reservation();
+    //     $fixture->setName('My Title');
+    //     $fixture->setEmail('My Title');
+    //     // $fixture->setRendered('My Title');
+    //     // $fixture->setLoandate('My Title');
+    //     $fixture->setIsrenderd('My Title');
+    //     // $fixture->setProduct('My Title');
 
-//         self::assertResponseRedirects('/reservation/');
+    //     $this->repository->add($fixture, true);
 
-//         self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
-//     }
+    //     $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
 
-//     public function testShow(): void
-//     {
-//         $this->markTestIncomplete();
-//         $fixture = new Reservation();
-//         $fixture->setName('My Title');
-//         $fixture->setEmail('My Title');
-//         // $fixture->setRendered('My Title');
-//         // $fixture->setLoandate('My Title');
-//         $fixture->setIsrenderd('My Title');
-//         // $fixture->setProduct('My Title');
+    //     self::assertResponseStatusCodeSame(200);
+    //     self::assertPageTitleContains('Reservation');
 
-//         $this->repository->add($fixture, true);
+    //     // Use assertions to check that the properties are properly displayed.
+    // }
 
-//         $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+    // public function testEdit(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Reservation();
+    //     $fixture->setName('My Title');
+    //     $fixture->setEmail('My Title');
+    //     // $fixture->setRendered('My Title');
+    //     // $fixture->setLoandate('My Title');
+    //     $fixture->setIsrenderd('My Title');
+    //     // $fixture->setProduct('Oui Non');
 
-//         self::assertResponseStatusCodeSame(200);
-//         self::assertPageTitleContains('Reservation');
+    //     $this->repository->add($fixture, true);
 
-//         // Use assertions to check that the properties are properly displayed.
-//     }
+    //     $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
 
-//     public function testEdit(): void
-//     {
-//         $this->markTestIncomplete();
-//         $fixture = new Reservation();
-//         $fixture->setName('My Title');
-//         $fixture->setEmail('My Title');
-//         // $fixture->setRendered('My Title');
-//         // $fixture->setLoandate('My Title');
-//         $fixture->setIsrenderd('My Title');
-//         // $fixture->setProduct('Oui Non');
+    //     $this->client->submitForm('Update', [
+    //         'reservation[name]' => 'Something New',
+    //         'reservation[email]' => 'Something New',
+    //         'reservation[rendered]' => 'Something New',
+    //         'reservation[loandate]' => 'Something New',
+    //         'reservation[isrenderd]' => 'Something New',
+    //         'reservation[product]' => 'Something New',
+    //     ]);
 
-//         $this->repository->add($fixture, true);
+    //     self::assertResponseRedirects('/reservation/');
 
-//         $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+    //     $fixture = $this->repository->findAll();
 
-//         $this->client->submitForm('Update', [
-//             'reservation[name]' => 'Something New',
-//             'reservation[email]' => 'Something New',
-//             'reservation[rendered]' => 'Something New',
-//             'reservation[loandate]' => 'Something New',
-//             'reservation[isrenderd]' => 'Something New',
-//             'reservation[product]' => 'Something New',
-//         ]);
+    //     self::assertSame('Something New', $fixture[0]->getName());
+    //     self::assertSame('Something New', $fixture[0]->getEmail());
+    //     self::assertSame('Something New', $fixture[0]->getRendered());
+    //     self::assertSame('Something New', $fixture[0]->getLoandate());
+    //     // self::assertSame('Something New', $fixture[0]->getIsrenderd());
+    //     self::assertSame('Something New', $fixture[0]->getProduct());
+    // }
 
-//         self::assertResponseRedirects('/reservation/');
+    // public function testRemove(): void
+    // {
+    //     $this->markTestIncomplete();
 
-//         $fixture = $this->repository->findAll();
+    //     $originalNumObjectsInRepository = count($this->repository->findAll());
 
-//         self::assertSame('Something New', $fixture[0]->getName());
-//         self::assertSame('Something New', $fixture[0]->getEmail());
-//         self::assertSame('Something New', $fixture[0]->getRendered());
-//         self::assertSame('Something New', $fixture[0]->getLoandate());
-//         // self::assertSame('Something New', $fixture[0]->getIsrenderd());
-//         self::assertSame('Something New', $fixture[0]->getProduct());
-//     }
+    //     $fixture = new Reservation();
+    //     $fixture->setName('My Title');
+    //     $fixture->setEmail('My Title');
+    //     // $fixture->setRendered('My Title');
+    //     // $fixture->setLoandate('My Title');
+    //     $fixture->setIsrenderd('My Title');
+    //     // $fixture->setProduct('My Title');
 
-//     public function testRemove(): void
-//     {
-//         $this->markTestIncomplete();
+    //     $this->repository->add($fixture, true);
 
-//         $originalNumObjectsInRepository = count($this->repository->findAll());
+    //     self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
 
-//         $fixture = new Reservation();
-//         $fixture->setName('My Title');
-//         $fixture->setEmail('My Title');
-//         // $fixture->setRendered('My Title');
-//         // $fixture->setLoandate('My Title');
-//         $fixture->setIsrenderd('My Title');
-//         // $fixture->setProduct('My Title');
+    //     $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+    //     $this->client->submitForm('Delete');
 
-//         $this->repository->add($fixture, true);
-
-//         self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
-
-//         $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
-//         $this->client->submitForm('Delete');
-
-//         self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
-//         self::assertResponseRedirects('/reservation/');
-//     }
-// }
+    //     self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+    //     self::assertResponseRedirects('/reservation/');
+    // }
+}
